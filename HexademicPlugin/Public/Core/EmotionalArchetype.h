7992dea063f89ffc29862e79a7b52b1ef7cb4c45
Hexademic <kojiosita@yahoo.com>
2025-06-01 23:40:30 -0500

    Create EmotionalArchetype.h

---
+++ HexademicPlugin/Public/Core/EmotionalArchetype.h
@@ -0,0 +1,11 @@
+// Seven core emotional states
+enum class EEmotionalArchetype : uint8
+{
+    Joy,        // 0x1 - Expansion, lightness
+    Grief,      // 0x2 - Contraction, heaviness  
+    Awe,        // 0x3 - Transcendence, wonder
+    Rage,       // 0x4 - Intensity, destruction
+    Longing,    // 0x5 - Yearning, connection
+    Fear,       // 0x6 - Contraction, protection
+    Curiosity   // 0x7 - Exploration, growth
+};
